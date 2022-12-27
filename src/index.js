75e22bd0fb45140fe8c3317cb9f888e7d3d1d8cd
David Dufresne <118769743+davidadufresne@users.noreply.github.com>
2022-12-27 13:52:23 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {createRoot}from 'react-dom/client';
+import './index.css';
+//import Card from './Card';
+import App from './Containers/App';
+//import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots';
+
+ 
+const root = createRoot(document.getElementById('root'));
+root.render(
+          <App robots={robots}/>
+);
+ 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
